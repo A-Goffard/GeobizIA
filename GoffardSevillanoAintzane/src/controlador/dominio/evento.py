5cb8389771c5b6480a632688e0b54911d255b9c0
A-Goffard <aintzagoffard@gmail.com>
2025-05-26 13:27:19 +0200

    eventos añadido

---
+++ GoffardSevillanoAintzane/src/controlador/dominio/evento.py
@@ -0,0 +1,61 @@
+class Evento:
+    def __init__(self, id, nombre, tipo, lugar, fecha_comienzo, fecha_final, poblacion, tematica):
+        self._id = id
+        self._nombre = nombre
+        self._tipo = tipo
+        self._lugar = lugar
+        self._fecha_comienzo = fecha_comienzo
+        self._fecha_final = fecha_final
+        self._poblacion = poblacion
+        self._tematica = tematica
+
+    @staticmethod
+    def crear_evento(id, nombre, tipo, lugar, fecha_comienzo, fecha_final, poblacion, tematica):
+        return Evento(id, nombre, tipo, lugar, fecha_comienzo, fecha_final, poblacion, tematica)
+
+    @property
+    def id(self): return self._id
+    @id.setter
+    def id(self, id): self._id = id
+
+    @property
+    def nombre(self): return self._nombre
+    @nombre.setter
+    def nombre(self, nombre): self._nombre = nombre
+
+    @property
+    def tipo(self): return self._tipo
+    @tipo.setter
+    def tipo(self, tipo): self._tipo = tipo
+
+    @property
+    def lugar(self): return self._lugar
+    @lugar.setter
+    def lugar(self, lugar): self._lugar = lugar
+
+    @property
+    def fecha_comienzo(self): return self._fecha_comienzo
+    @fecha_comienzo.setter
+    def fecha_comienzo(self, fecha_comienzo): self._fecha_comienzo = fecha_comienzo
+
+    @property
+    def fecha_final(self): return self._fecha_final
+    @fecha_final.setter
+    def fecha_final(self, fecha_final): self._fecha_final = fecha_final
+
+    @property
+    def poblacion(self): return self._poblacion
+    @poblacion.setter
+    def poblacion(self, poblacion): self._poblacion = poblacion
+
+    @property
+    def tematica(self): return self._tematica
+    @tematica.setter
+    def tematica(self, tematica): self._tematica = tematica
+
+    def __str__(self):
+        return (
+            f"ID: {self.id}, Nombre: {self.nombre}, Tipo: {self.tipo}, Lugar: {self.lugar}, "
+            f"Fecha comienzo: {self.fecha_comienzo}, Fecha final: {self.fecha_final}, "
+            f"Población: {self.poblacion}, Temática: {self.tematica}"
+        )
---
+++ GoffardSevillanoAintzane/src/controlador/gestores/eventos.py
@@ -0,0 +1,6 @@
+from gestores.base_gestor import BaseGestor
+
+class Eventos(BaseGestor):
+    # No es necesario redefinir los métodos agregar, modificar, buscar o eliminar
+    # porque ya están implementados en BaseGestor.
+    pass
