daa67b729125f2ccc7db5f3498d58242f7fb1e1c
A-Goffard <aintzagoffard@gmail.com>
2025-06-20 14:31:51 +0200

    un cambio estructural de algunos gestores

--- GoffardSevillanoAintzane/src/controlador/gestores/auditorias_publicaciones.py
+++ GoffardSevillanoAintzane/src/controlador/gestores/auditorias_publicaciones.py
@@ -1,7 +1,7 @@
-from abc import ABC, abstractmethod
 from dominios.auditoria_publicacion import AuditoriaPublicacion
+from gestores.base_gestor import BaseGestor
 
-class AuditoriaPublicacion(ABC):
-    @abstractmethod
-    def crear_auditoria(self, datos) -> AuditoriaPublicacion:
-        pass
+class AuditoriasPublicaciones(BaseGestor):
+    # No es necesario redefinir los métodos agregar, modificar, buscar o eliminar
+    # porque ya están implementados en BaseGestor.
+    pass
--- GoffardSevillanoAintzane/src/controlador/gestores/logs_sistema.py
+++ GoffardSevillanoAintzane/src/controlador/gestores/logs_sistema.py
@@ -1,7 +1,7 @@
-from abc import ABC, abstractmethod
 from dominios.log_sistema import LogSistema
+from gestores.base_gestor import BaseGestor
 
-class LogSistema(ABC):
-    @abstractmethod
-    def crear_log(self, datos) -> LogSistema:
-        pass
+class LogsSistema(BaseGestor):
+    # No es necesario redefinir los métodos agregar, modificar, buscar o eliminar
+    # porque ya están implementados en BaseGestor.
+    pass
